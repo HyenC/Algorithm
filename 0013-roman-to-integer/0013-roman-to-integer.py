46763a2dd78cd35b3c0cfedc8cded9e069f2906e
HyenC <38906574+HyenC@users.noreply.github.com>
2023-09-06 00:39:38 +0900

    Update 0013-roman-to-integer.py
    Time: 52 ms (68.93%), Space: 16.4 MB (47.26%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,20 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        num = {
-            'I' : '1a',
-            'V' : '5a',
-            'X' : '10a',
-            'L' : '50a',
-            'C' : '100a',
-            'D' : '500a',
-            'M' : '1000a',
-            'CM' : '900a',
-            'CD' : '400a',
-            'XC' : '90a',
-            'XL' : '40a',
-            'IX' : '9a',
-            'IV' : '4a'
+        num_dict = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000,
+            'CM' : 900,
+            'CD' : 400,
+            'XC' : 90,
+            'XL' : 40,
+            'IX' : 9,
+            'IV' : 4
         }
-        for r in reversed(num):
-            s = s.replace(r, num[r])
-        return sum(map(int, s.split('a')[:-1]))
+        total = []
+        for r in reversed(num_dict):
+            while r in s:
+                total.append(num_dict[r])
+                s = s.replace(r, '', 1)
+        return sum(total)
